f1fd85199f61dfaae76aa97e8e20f6c8c5bf1693
Rich <r.j.lewindon@hotmail.co.uk>
2020-02-20 12:03:22 +0000

    Allocated buttons to variables in MainActivity

--- app/src/main/java/com/example/android/historyapp/MainActivity.java
+++ app/src/main/java/com/example/android/historyapp/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.android.historyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mFindSitesNearMeButton;
+    Button mSeeAllSitesButton;
+    Button mSeeSitesOnMapButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFindSitesNearMeButton = findViewById(R.id.findSitesNearMeButton);
+        mSeeAllSitesButton = findViewById(R.id.seeAllSitesButton);
+        mSeeSitesOnMapButton = findViewById(R.id.findSitesNearMeButton);
     }
 }
