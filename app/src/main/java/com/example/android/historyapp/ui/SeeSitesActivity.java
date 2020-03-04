58db911116a78263b02e934774667bbfc11d336e
Rich <r.j.lewindon@hotmail.co.uk>
2020-03-04 12:05:40 +0000

    Sets rating on both tablet and phone

--- app/src/main/java/com/example/android/historyapp/ui/SeeSitesActivity.java
+++ app/src/main/java/com/example/android/historyapp/ui/SeeSitesActivity.java
@@ -49,7 +49,7 @@ public class SeeSitesActivity extends AppCompatActivity implements SitesAdapter.
             arguments.putString(SitesDetailsFragment.ARG_ITEM_ID, SitesListFragment.dummyD.get(i).getName());
             arguments.putString(SitesDetailsFragment.ARG_ITEM_HISTORY, SitesListFragment.dummyD.get(i).getHistory());
             arguments.putString(SitesDetailsFragment.ARG_ITEM_OPERATOR, SitesListFragment.dummyD.get(i).getOperator());
-            arguments.putDouble(SitesDetailsFragment.ARG_ITEM_RATING, SitesListFragment.dummyD.get(i).getRating());
+            arguments.putInt(SitesDetailsFragment.ARG_ITEM_RATING, SitesListFragment.dummyD.get(i).getRating());
             SitesDetailsFragment fragment = new SitesDetailsFragment();
             fragment.setArguments(arguments);
 
